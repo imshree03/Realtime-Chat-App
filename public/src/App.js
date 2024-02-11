ee965f8509bf11a836870a474d80ee5bf3cd5d30
imshree03 <shreebhosale24hrs@gmail.com>
2024-02-11 11:36:11 +0530

    Routes crreated inside   App.js commited

---
+++ public/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
+import SetAvatar from "./components/SetAvatar";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Chat />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
