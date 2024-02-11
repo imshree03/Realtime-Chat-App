aa2e47ae9be77058a309065ffe867597e544a87b
imshree03 <shreebhosale24hrs@gmail.com>
2024-02-11 11:39:39 +0530

    Auth Routes commited

---
+++ server/routes/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {
+  register,
+  login,
+  setAvatar,
+  getAllUsers,
+} = require("../controllers/userController");
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/setAvatar/:id", setAvatar);
+router.get("/allusers/:id", getAllUsers);
+module.exports = router;
